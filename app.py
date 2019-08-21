7607495b2a18a7eb8a9b477f2282b6e7068f679b
am <ashutoshmishra413@gmail.com>
2019-08-20 20:51:34 -0400

    Added TODO's

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# TODO: create function that grabs twitch data & run it every 'x' minutes
+#   TODO: Make sure that data from function is stored in postgres
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +9,7 @@ app = Flask(__name__)
 # Twitch analytics app
 @app.route("/project/twitch-analytics/app")
 def twitch_analytics_app():
+
     return "analytics"
 
 
